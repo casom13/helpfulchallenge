3123dfb069ce2e1e6d1b87fcf80d8ec7e9ab7a93
casey <caseyisom13@gmail.com>
2021-07-22 21:47:40 -0600

    Fixing styles, enabled the sidebar menu by adding a new field to the colors table called match. This uses math to match each hex with the closest color avail in the menu, added search bar on top but it currently only works with colors

--- models.js
+++ models.js
@@ -6,13 +6,61 @@ const db = new Sequelize({
 });
 
 const Color = db.define("color", {
-    hex: { type: Sequelize.STRING }
+    hex: { type: Sequelize.STRING },
+    match: { type: Sequelize.STRING }
 });
 
 db.sync();
 
+// define available colors
+let colors = {
+    '6600ff': 'purple',
+    '808080': 'gray',
+    '0000ff': 'blue',
+    '663300': 'brown',
+    '00ff00': 'green',
+    'ff8000': 'orange',
+    'ff0000': 'red',
+    'ffff00': 'yellow',
+};
+
+// convert the `colors`-object to an array
+let colorsArr = [];
+for(let key in colors) {
+    colorsArr.push(key);
+}
+
+// Function used to find the closest color
+Array.closest = (function () {
+    function dist(s, t) {
+        if (!s.length || !t.length) return 0;
+        return dist(s.slice(2), t.slice(2)) +
+            Math.abs(parseInt(s.slice(0, 2), 16) - parseInt(t.slice(0, 2), 16));
+    }
+
+    return function (arr, str) {
+        var min = 0xffffff;
+        var best, current, i;
+        for (i = 0; i < arr.length; i++) {
+            current = dist(arr[i], str)
+            if (current < min) {
+                min = current
+                best = arr[i];
+            }
+        }
+        return best;
+    };
+}());
+
 for (let i = 0; i < 100; i++) {
-    Color.create({ hex: '#'+(Math.random()*0xFFFFFF<<0).toString(16) })
+    let hex = (Math.random()*0xFFFFFF<<0).toString(16);
+    while (hex.length < 6){
+        hex = "0"+hex
+    }
+    let match = Array.closest(colorsArr, hex);
+    //console.log(hex + ' matches to ' + colors[match])
+
+    Color.create({ hex: '#'+hex, match: colors[match] })
 }
 
 db.sync();
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const models = require("../models");
+const { Op } = require("sequelize");
 
 router.get('/', function(req, res) {
   const offset = 0;
@@ -8,7 +9,8 @@ router.get('/', function(req, res) {
     const count = (c / 12) + 1;
     models.Color.findAll({
       limit: 12,
-      offset: offset
+      offset: offset,
+      order: [['id', 'DESC']]
     }).then(function (colors) {
       res.render('index', {
         title: 'Interview Challenge',
@@ -41,6 +43,33 @@ router.get('/page/:page?', function(req, res) {
         page: page
       });
       console.log(colors.length);
+    });
+  });
+});
+
+router.get('/search/:term?/:page?', function(req, res) {
+  let term = (req.params.term) ? req.params.term : req.query.term;
+  let page = req.params.page;
+  let offset = 0;
+  if (!page){
+    page = 1;
+  } else{
+    offset = (req.params.page - 1) * 12;
+  }
+  models.Color.count({where: { match: { [Op.like]: term} }}).then(c => {
+    const count = (c / 12) + 1;
+    models.Color.findAll({
+      where: { match: { [Op.like]: term} },
+      limit: 12,
+      offset: offset
+    }).then(function (colors) {
+      res.render('index', {
+        title: 'Interview Challenge',
+        colors: colors,
+        count: count,
+        page: page,
+        term: term
+      });
     });
   });
 });
